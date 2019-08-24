6de5755a3ce1a1599d13d85ab68bd634d524549c
Alexander <labianca.alex@gmail.com>
2019-08-24 16:52:45 -0500

    certificate endpoint

--- authority/authd/main.go
+++ authority/authd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -40,55 +39,56 @@ func main() {
 	}
 
 	vaultToken := os.Getenv("VAULT_DEV_TOKEN")
+	vaultURL := os.Getenv("VAULT_URL")
 
-	http.Handle("/certificate", certificate(vaultToken))
+	http.Handle("/certificate", certificate(vaultToken, vaultURL))
 
 	if err := http.ListenAndServe(getListenAddr(), nil); err != nil {
 		log.Println(err)
 	}
 
-	client := &http.Client{}
-	csr := &CSR{
-		CommonName: "lucky-goat.snfs.com",
-		TTL:        "24h",
-	}
+	// client := &http.Client{}
+	// csr := &CSR{
+	// 	CommonName: "lucky-goat.snfs.com",
+	// 	TTL:        "24h",
+	// }
 
-	bts, err := json.Marshal(csr)
-	if err != nil {
-		panic(err)
-	}
+	// bts, err := json.Marshal(csr)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	buf := bytes.NewBuffer(bts)
-	fmt.Println("Sending CSR")
-	fmt.Println(buf.String())
+	// buf := bytes.NewBuffer(bts)
+	// fmt.Println("Sending CSR")
+	// fmt.Println(buf.String())
 
-	req, err := http.NewRequest("POST", "http://127.0.0.1:8200/v1/pki_int/issue/snfs-dot-com", buf)
-	if err != nil {
-		panic(err)
-	}
+	// req, err := http.NewRequest("POST", vaultURL, buf)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	req.Header.Add("X-Vault-Token", vaultToken)
-	resp, err := client.Do(req)
+	// req.Header.Add("X-Vault-Token", vaultToken)
+	// resp, err := client.Do(req)
 
-	if err != nil {
-		panic(err)
-	}
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	defer resp.Body.Close()
+	// defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	var csrResp CSRResponse
-	if err := json.Unmarshal(body, &csrResp); err != nil {
-		panic(err)
-	}
+	// var csrResp CSRResponse
+	// if err := json.Unmarshal(body, &csrResp); err != nil {
+	// 	panic(err)
+	// }
 
-	fmt.Println(csrResp.Data.Certificate)
-	fmt.Println(csrResp.Data.PrivateKey)
-	fmt.Println(csrResp.Data.IssuingCA)
+	// fmt.Println(csrResp.Data.Certificate)
+	// fmt.Println(csrResp.Data.PrivateKey)
+	// fmt.Println(csrResp.Data.IssuingCA)
 
 }
 
@@ -97,8 +97,44 @@ func getListenAddr() string {
 	return ":" + port
 }
 
-func certificate(vaultToken string) http.HandlerFunc {
-	return func(res http.ResponseWriter, req *http.Request) {
+func certificate(vaultToken, vaultURL string) http.HandlerFunc {
+	return func(res http.ResponseWriter, r *http.Request) {
+		csr, err := parseRequestBody(r)
+		if err != nil {
+			res.WriteHeader(400)
+			res.Write([]byte(err.Error()))
+			return
+		}
+
+		log.Printf("Getting Certificate for %s\n", csr.CommonName)
+		client := &http.Client{}
+		vReq, err := vaultRequest(csr, vaultURL, vaultToken)
+		if err != nil {
+			res.WriteHeader(400)
+			res.Write([]byte(err.Error()))
+			return
+		}
+
+		resp, err := client.Do(vReq)
+
+		if err != nil {
+			res.WriteHeader(400)
+			res.Write([]byte(err.Error()))
+			return
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			res.WriteHeader(400)
+			res.Write([]byte(err.Error()))
+			return
+		}
+
+		res.Header().Set("Content-Type", "application/json")
+		res.WriteHeader(http.StatusOK)
+		res.Write(body)
 
 	}
 }
@@ -125,4 +161,22 @@ func parseRequestBody(request *http.Request) (CSR, error) {
 	decoder.Decode(&csr)
 	return csr, err
 
+}
+
+func vaultRequest(csr CSR, url, token string) (*http.Request, error) {
+	bts, err := json.Marshal(csr)
+	if err != nil {
+		return nil, err
+	}
+
+	buf := bytes.NewBuffer(bts)
+
+	req, err := http.NewRequest("POST", url, buf)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("X-Vault-Token", token)
+
+	return req, nil
 }
