dc24a0e8f96b39f7963511c2a8badd8fb9e11d21
Alexander <labianca.alex@gmail.com>
2019-11-12 08:22:48 -0600

    Resolve Service

--- snfs/server/rest.go
+++
@@ -1,5 +0,0 @@
-package server
-
-type Rest interface {
-	REST() error
-}
--- snfs/server/server.go
+++ snfs/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"sync"
@@ -9,10 +10,13 @@ import (
 const NumServices = 5
 
 var queue chan Service
-var once sync.Once
+var onceQueue sync.Once
+var onceInstance sync.Once
+var serverInstance *Server
+var mtx = sync.Mutex{}
 
 func InitQueue(maxSize int) {
-	once.Do(func() {
+	onceQueue.Do(func() {
 		queue = make(chan Service, maxSize)
 	})
 }
@@ -21,8 +25,24 @@ func QueueService(s Service) {
 	queue <- s
 }
 
+func ResolveService(token string) (Service, error) {
+	mtx.Lock()
+	defer mtx.Unlock()
+	if serverInstance == nil {
+		return nil, errors.New(ErrServiceNotResolved)
+	}
+
+	s, ok := serverInstance.services[token]
+	if !ok {
+		return nil, errors.New(ErrServiceNotResolved)
+	}
+
+	return s.service, nil
+}
+
 // Errors
 const ErrServerNotSet = "Server Not Set"
+const ErrServiceNotResolved = "Service Not Resolved"
 
 type serviceEntry struct {
 	started bool
@@ -50,19 +70,21 @@ type Server struct {
 }
 
 func New(port int, host string) *Server {
-	serverInstace := &Server{
-		Port:         port,
-		Addr:         host,
-		lock:         sync.Mutex{},
-		services:     make(map[string]serviceEntry),
-		startService: make(chan Service, NumServices),
-		stopService:  make(chan Service, 1),
-		stopAll:      make(chan bool),
-		exit:         make(chan error),
-		exitService:  make(chan string),
-	}
+	onceInstance.Do(func() {
+		serverInstance = &Server{
+			Port:         port,
+			Addr:         host,
+			lock:         sync.Mutex{},
+			services:     make(map[string]serviceEntry),
+			startService: make(chan Service, NumServices),
+			stopService:  make(chan Service, 1),
+			stopAll:      make(chan bool),
+			exit:         make(chan error),
+			exitService:  make(chan string),
+		}
+	})
 
-	return serverInstace
+	return serverInstance
 }
 
 func (s *Server) StartService(service Service) {
